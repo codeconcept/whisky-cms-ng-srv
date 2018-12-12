ba94615428bcb7f06bd4c85c54f4b55f35e02907
codeconcept <contact@codeconcept.fr>
2018-12-12 18:50:08 +0100

    deleting single document and several documents

--- api/v1/index.js
+++ api/v1/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const Blogpost = require('../models/blogpost');
+const mongoose = require('mongoose');
 
 router.get('/ping', (req, res) => {
 	res.status(200).json({ msg: 'pong', date: new Date()});
@@ -35,6 +36,39 @@ router.post('/blog-posts', (req, res) => {
 			return res.status(500).json(err);
 		}
 		res.status(201).json(blogPost);
+	});
+});
+
+router.delete('/blog-posts/:id', (req, res) => {
+	const id = req.params.id;
+	console.log('delete by id', id);
+	Blogpost.findByIdAndDelete(id, (err, blogPost) => {
+		if (err) {
+			return res.status(500).json(err);
+		}
+		res.status(202).json({ msg: `blog post with id ${blogPost._id} deleted`});
+	});
+});
+
+router.delete('/blog-posts', (req, res) => {
+	// retrieves the query parameter: http://localhost:3000/api/v1/blog-posts?ids=5c1133b8225e420884687048,5c1133b6225e420884687047
+	const ids = req.query.ids;
+	console.log('query allIds', ids);
+	const allIds = ids.split(',').map(id => {
+		// casting as a mongoose ObjectId	
+		if (id.match(/^[0-9a-fA-F]{24}$/)) {
+			return mongoose.Types.ObjectId((id));		 
+		}else {
+			console.log('id is not valid', id);
+			return -1;
+		}
+	});
+	const condition = { _id: { $in: allIds} };
+	Blogpost.deleteMany(condition, (err, result) => {
+		if (err) {
+			return res.status(500).json(err);
+		}
+		res.status(202).json(result);
 	});
 });
 
